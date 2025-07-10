e7c15e3867991df87b480057606bfb5137951e32
Евгений <brearey4@gmail.com>
2025-07-10 18:16:08 +0900

    create-task-wrapper categories-wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,16 @@ function App() {
   return (
     <>
       <div className="bg">
-          <div className="container">
+          <div className="container-w">
               <h1>Todo app</h1>
-              <div className="create-task">
+              <div className="create-task-wrapper">
                   <input type="text" placeholder="Create task"/>
                   <button>Submit</button>
+              </div>
+              <div className="categories-wrapper">
+                  <div className="category-button active">All</div>
+                  <div className="category-button">Active</div>
+                  <div className="category-button">Completed</div>
               </div>
           </div>
       </div>
